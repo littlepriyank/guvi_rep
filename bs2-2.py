78fff903ebdb63da5942601a0186b238b129c81d
littlepriyank <35061236+littlepriyank@users.noreply.github.com>
2018-01-16 23:00:40 +0530

    Add files via upload

---
+++ bs2-2.py
@@ -0,0 +1,12 @@
+N = int(input("enter a number"))
+if(N<=1000):
+    p = 0;x=N
+    while(x!=0):
+        p = p*10 + x%10
+        x=int(x/10)
+    if(p==N):
+        print("yes")
+    else:
+        print("no")
+else:
+    print("number must be less than 1000")
