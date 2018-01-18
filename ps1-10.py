60cbdad2fa30d79e8b54d3b83eb05af615e5199f
littlepriyank <35061236+littlepriyank@users.noreply.github.com>
2018-01-18 21:13:47 +0530

    Add files via upload

---
+++ ps1-10.py
@@ -0,0 +1,15 @@
+p,q = [x for x in input().split(" ")]
+j=0;c=0
+if((len(p)and len(q))<=100000):
+    if(len(p)==len(q)):
+        for i in p:
+            if(i==q[j]):
+                j+=1
+            else:
+                 j+=1
+                 c+=1
+            if(c>1):
+                  print("NO")
+                  break
+        if(c==1):
+             print("yes")         
