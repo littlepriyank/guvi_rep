ba5a605d8d07820af3466d7ff9fa2d912b7f74d5
littlepriyank <35061236+littlepriyank@users.noreply.github.com>
2018-04-15 16:27:09 +0530

    Add files via upload

---
+++ hunter1-1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+    int n,*a,x,l=0,flag=0;
+	scanf("%d",&n);
+	a =(int *) malloc(sizeof(int)*n);
+	for(int i=0;i<n;i++){
+	    scanf("%d",&x);
+	    l = (l>x?l:x);
+	    a[x]++;
+	    if(a[x]>1){
+	        flag=1;
+	    }
+	}
+	if(flag==0){
+	        printf("unique");
+	}
+	else{
+	for(int i=0;i<l;i++){
+	  if(a[i]>1)
+	  printf("%d ",i);
+	  
+	}}
+	return 0;
+}
+
